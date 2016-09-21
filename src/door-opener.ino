17291200a13c6b69006b5d0098eddf05bc9472ef
Louie Adamian <LouieAdamian@users.noreply.github.com>
2016-09-21 13:24:06 -0400

    adds libraries

---
+++ src/door-opener.ino
@@ -0,0 +1,18 @@
+#include "Due.h"
+#include "MifareClassic.h"
+#include "MifareUltralight.h"
+#include "Ndef.h"
+#include "NdefMessage.h"
+#include "NdefRecord.h"
+#include "NfcAdapter.h"
+#include "NfcDriver.h"
+#include "NfcTag.h"
+#include "SPI.h"
+#include "PN532_SPI.h"
+#include "snep.h"
+
+void setup() {
+}
+void loop() {
+
+}
